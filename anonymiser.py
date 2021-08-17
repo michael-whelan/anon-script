bedd9fbe4c5e29f4cab30e4b4f7629fced7f17bf
Michael Whelan <michael.whelan@ly.st>
2021-08-17 11:53:14 +0100

    Delete outfile, read infile, create new outfile

---
+++ anonymiser.py
@@ -0,0 +1,45 @@
+
+import sys
+from os import path, remove
+
+out_file_name = "anon_out.txt"
+
+
+def check_and_delete_out_file():
+    out_file = path.exists(out_file_name)
+    print ("File exists:" + str(out_file))
+    if out_file:
+        print (f"Deleting old {out_file_name}...")
+        remove(out_file_name)
+
+def get_from_in_file():
+    email_list =[]
+    with open(sys.argv[1]) as f:
+        for line in f:
+            email_list.append(line.rstrip())
+    email_list = list(dict.fromkeys(email_list))
+    return email_list
+
+def write_out_file(email_list):
+    try:
+        joined_string = " ".join(email_list)
+        f = open(out_file_name, "a")
+        f.write(joined_string)
+        return True
+    except:
+        return False
+
+def main():
+    if len(sys.argv) != 2:
+        print("No file given. Please specify an input file")
+        return []
+    check_and_delete_out_file()
+    email_list = get_from_in_file()
+    if len(email_list) > 0:
+        if write_out_file(email_list):
+            print(f"Success {out_file_name} generated with {len(email_list)} unique emails")
+        else:
+            print("error")
+
+
+main()
